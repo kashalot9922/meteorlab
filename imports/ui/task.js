7f6a89095495e845b273510d635eb778e52d87ef
kashalot9922 <vladm1996@gmail.com>
2016-12-07 23:18:14 +0300

    9.Security with methods

--- imports/ui/task.js
+++ imports/ui/task.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
  
 import { Tasks } from '../api/tasks.js';
@@ -7,11 +8,12 @@ import './task.html';
 Template.task.events({
   'click .toggle-checked'() {
     // Set the checked property to the opposite of its current value
+	    Meteor.call('tasks.setChecked', this._id, !this.checked);
     Tasks.update(this._id, {
       $set: { checked: ! this.checked },
     });
   },
   'click .delete'() {
-    Tasks.remove(this._id);
+      Meteor.call('tasks.remove', this._id);
   },
 });
